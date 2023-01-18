e9503c2e1f7fd1715634dcacc0b5d5ab3378efcb
Yin-Te Lan <k0960355356@gmail.com>
2023-01-18 13:27:21 +0800

    <piano link>

---
+++ components/ProjectCard/index.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/components/ProjectCard/ProjectCard.module.scss'
+import { hostname } from 'os'
+
+
+
+export default function Home() {
+
+    
+
+  return (
+    <>
+      <div onClick={()=>{
+            window.open(location.host + '/piano-with-react')
+        }} className={styles.project_card_wrapper}>
+            <h1>
+                piano-with-react
+            </h1>
+      </div>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import ProjectCard from '@/components/ProjectCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,7 @@ export default function Home() {
        
         <img src="/blue-jay.png" alt="" />
        
-
+        <ProjectCard/>
     
       </main>
     </>
